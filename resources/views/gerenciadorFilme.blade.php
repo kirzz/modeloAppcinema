dc8d1c58c753dade8cb02c204148df70f5954151
kirzz <kaick.filipe.maria@gmail.com>
2023-05-08 22:24:54 -0300

    ATT0805

--- resources/views/gerenciadorFilme.blade.php
+++ resources/views/gerenciadorFilme.blade.php
@@ -41,12 +41,12 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Tem certeza de que deseja excluir?</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">NÃO</button>
+        <button type="button" class="btn btn-primary">SIM</button>
       </div>
     </div>
   </div>
